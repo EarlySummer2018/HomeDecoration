d4246e0f8fa8eb819faf618b0addda4b066d7209
EarlySummer2018 <1687049214@qq.com>
2023-09-10 22:35:29 +0800

    🎉：优化轮播图 hook

--- src/hooks/useSwiper.ts
+++ src/hooks/useSwiper.ts
@@ -1,56 +1,44 @@
-import { SwiperItem, SwiperStyle } from "@/interface/swiper";
-import { computed, ref, watch } from "vue";
+import { useTemplate } from "@/store";
+import { ref, watch } from "vue";
 
-const useSwiper = (templateStore:any) => {
-  const data = templateStore.currentHanderObject
-  // 轮播图公共样式
-  const swiperStyle:SwiperStyle = data.options;
-
-  const titleStartColor = ref<string>(data.options.titleBgColor)
-  const titleEndColor = ref<string>(data.options.titleBgColor)
-
-  // 轮播图数据
-  const swiper = computed<SwiperItem[]>({
-    get() {
-      return data.value
-    },
-    set(nVal) {
-      const obj = data
-      obj.value = nVal
-      obj.options = swiperStyle
-      templateStore.setCurrentHandlerObject(obj)
-    }
-  });
+const useSwiper = () => {
+  const {currentHanderObject} = useTemplate()
+  
+  const {value: swiper, style, options} = currentHanderObject.data;
+  
+  const titleStartColor = ref<string>(style.titleBgColor)
+  const titleEndColor = ref<string>(style.titleBgColor)
 
   /**
    * 根据索引删除轮播图
    * @param index 要移除的轮播图索引
    */
   const removeSwiper = (index:number) => {
-    swiper.value.splice(index, 1)
+    swiper.splice(index, 1)
   }
 
   watch([titleStartColor, titleEndColor],([start, end])=>{
-    if (swiperStyle.titleBgType === 1) {
-      swiperStyle.titleBgColor = start
-    }else if (swiperStyle.titleBgType === 2) {
-      swiperStyle.titleBgColor = [start, end]
+    if (options.titleBgType === 1) {
+      style.titleBgColor = start
+    }else if (options.titleBgType === 2) {
+      style.titleBgColor = [start, end]
     }
   })
 
   watch(
-    () => swiperStyle.titleBgType,
+    () => options.titleBgType,
     (nVal) => {
       if (nVal === 1) {
-        swiperStyle.titleBgColor = titleStartColor.value
+        style.titleBgColor = titleStartColor.value
       }else{
-        swiperStyle.titleBgColor = [titleStartColor.value, titleEndColor.value]
+        style.titleBgColor = [titleStartColor.value, titleEndColor.value]
       }
   })
 
   return {
     swiper,
-    swiperStyle,
+    style,
+    options,
     titleStartColor,
     titleEndColor,
     removeSwiper
